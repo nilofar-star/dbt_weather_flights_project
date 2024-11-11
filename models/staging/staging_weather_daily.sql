d120210dae2453dbbedfd41d84ebe36116cc1926
nilofar-star <n.sarkhushzada1@gmail.com>
2024-11-11 17:12:56 +0100

    files5

--- models/staging/staging_weather_daily.sql
+++ models/staging/staging_weather_daily.sql
@@ -1,27 +1,25 @@
-WITH daily_data AS (
-    SELECT * 
-    FROM {{ref('staging_weather_daily')}}
+WITH daily_raw AS (
+    SELECT
+            airport_code,
+            station_id,
+            JSON_ARRAY_ELEMENTS(extracted_data -> 'data') AS json_data
+    FROM {{source('staging_weather', 'weather_daily_raw')}}
 ),
-add_features AS (
-    SELECT *
-		, ... AS date_day
-		, ... AS date_month
-		, ... AS date_year
-		, ... AS cw
-		, ... AS month_name
-		, ... AS weekday
-    FROM daily_data 
-),
-add_more_features AS (
-    SELECT *
-		, (CASE 
-			WHEN month_name in ... THEN 'winter'
-			WHEN ... THEN 'spring'
-            WHEN ... THEN 'summer'
-            WHEN ... THEN 'autumn'
-		END) AS season
-    FROM add_features
+daily_flattened AS (
+    SELECT  airport_code,
+            station_id,
+            (json_data->>'date')::DATE AS date,
+            (json_data->>'tavg')::NUMERIC AS avg_temp_c,
+            (json_data->>'tmin')::NUMERIC AS min_temp_c,
+            (json_data->>'tmax')::NUMERIC AS max_temp_c,
+            (json_data->>'prcp')::NUMERIC AS precipitation_mm,
+            ((json_data->>'snow')::NUMERIC)::INTEGER AS max_snow_mm,
+            ((json_data->>'wdir')::NUMERIC)::INTEGER AS avg_wind_direction,
+            (json_data->>'wspd')::NUMERIC AS avg_wind_speed_kmh,
+            (json_data->>'wpgt')::NUMERIC AS wind_peakgust_kmh,
+            (json_data->>'pres')::NUMERIC AS avg_pressure_hpa,
+            (json_data->>'tsun')::INTEGER AS sun_minutes
+    FROM daily_raw
 )
-SELECT *
-FROM add_more_features
-ORDER BY date
+SELECT * 
+FROM daily_flattened
