5f92b89a6325286ced876b96e7c121a362c62d74
nilofar-star <n.sarkhushzada1@gmail.com>
2024-11-11 17:45:08 +0100

    prep daily weather rebuild

--- models/prep/prep_weather_daily.sql
+++ models/prep/prep_weather_daily.sql
@@ -1,25 +1,25 @@
 WITH daily_data AS (
     SELECT * 
-    FROM {{ ref('staging_weather_daily') }}
+    FROM {{ref('staging_weather_daily')}}
 ),
 add_features AS (
     SELECT *
-        , DATE_PART('day', date) AS date_day
-        , DATE_PART('month', date) AS date_month
-        , DATE_PART('year', date) AS date_year
-        , EXTRACT(WEEK FROM date) AS cw  -- Adjusted for general SQL compatibility
-        , TO_CHAR(date, 'Month') AS month_name  -- For databases like PostgreSQL
-        , TO_CHAR(date, 'Day') AS weekday       -- For databases like PostgreSQL
+		, DATE_PART('day', date) AS date_day 		-- number of the day of month
+		, DATE_PART('month', date) AS date_month 	-- number of the month of year
+		, DATE_PART('year', date) AS date_year 		-- number of year
+		, DATE_PART('week', date) AS cw 			-- number of the week of year
+		, TO_CHAR(date, 'FMmonth') AS month_name 	-- name of the month
+		, TO_CHAR(date, 'FMday') AS weekday 		-- name of the weekday
     FROM daily_data 
 ),
 add_more_features AS (
     SELECT *
-        , (CASE
-            WHEN DATE_PART('month', date) IN (12, 1, 2) THEN 'winter'
-            WHEN DATE_PART('month', date) IN (3, 4, 5) THEN 'spring'
-            WHEN DATE_PART('month', date) IN (6, 7, 8) THEN 'summer'
-            WHEN DATE_PART('month', date) IN (9, 10, 11) THEN 'autumn'
-        END) AS season
+		, (CASE 
+			WHEN month_name in ('december','january','february') THEN 'winter'
+			WHEN month_name in ('march','april','may') THEN 'spring'
+            WHEN month_name in ('june','july','august') THEN 'summer'
+            WHEN month_name in ('september','october','november') THEN 'autumn'
+		END) AS season
     FROM add_features
 )
 SELECT *
