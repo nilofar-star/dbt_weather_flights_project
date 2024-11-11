c077f2f99a170560cad86933ab46d42f320841cb
nilofar-star <n.sarkhushzada1@gmail.com>
2024-11-11 16:35:05 +0100

    files

--- models/prep/prep_weather_hourly.sql
+++ models/prep/prep_weather_hourly.sql
@@ -1,27 +1,29 @@
-WITH hourly_raw AS (
-    SELECT
-            airport_code,
-            station_id,
-            JSON_ARRAY_ELEMENTS(extracted_data -> 'data') AS json_data
-    FROM  {{ref('staging_weather_hourly')}}
-),
-hourly_data AS (
-    SELECT  
-            airport_code
-            ,station_id
-            ,(json_data->>'time')::TIMESTAMP AS timestamp	
-            ,(json_data->>'temp')::NUMERIC AS temp_c
-            ,(json_data->>'dwpt')::NUMERIC AS dewpoint_c
-            ,(json_data->>'rhum')::NUMERIC AS humidity_perc
-            ,(json_data->>'prcp')::NUMERIC AS precipitation_mm
-            ,(json_data->>'snow')::INTEGER AS snow_mm
-            ,((json_data->>'wdir')::NUMERIC)::INTEGER AS wind_direction
-            ,(json_data->>'wspd')::NUMERIC AS wind_speed_kmh
-            ,(json_data->>'wpgt')::NUMERIC AS wind_peakgust_kmh
-            ,(json_data->>'pres')::NUMERIC AS pressure_hpa 
-            ,(json_data->>'tsun')::INTEGER AS sun_minutes
-            ,(json_data->>'coco')::INTEGER AS condition_code
-    FROM hourly_raw
-)
-SELECT * 
-FROM hourly_data
+WITH hourly_data AS (
+        SELECT * 
+        FROM {{ref('staging_weather_hourly')}}
+    ),
+    add_features AS (
+        SELECT *
+        , timestamp::DATE AS date -- only date (year-month-day) as DATE data type
+        , timestamp::TIME AS time -- only time (hours:minutes:seconds) as TIME data type
+            , TO_CHAR(timestamp,'HH24:MI') as hour -- time (hours:minutes) as TEXT data type
+            , TO_CHAR(timestamp, 'FMmonth') AS month_name -- name of the month
+            , TO_CHAR(timestamp, 'FMday') AS weekday -- name of the weekday       
+            , DATE_PART('day', timestamp) AS date_day
+    		, DATE_PART('month', timestamp) AS date_month
+    		, DATE_PART('year', timestamp) AS date_year
+    		, DATE_PART('week', timestamp) AS cw
+        FROM hourly_data
+    ),
+    add_more_features AS (
+        SELECT *
+    		,(CASE 
+    			WHEN time BETWEEN '00:00:00' AND '05:59:00' THEN 'night'
+    			WHEN time BETWEEN '06:00:00' AND '18:00:00' THEN 'day'
+    			WHEN time BETWEEN '18:00:00' AND '23:59:00' THEN 'evening'
+    		END) AS day_part
+        FROM add_features
+    )
+    
+    SELECT *
+    FROM add_more_features
